2f6003244f64c096c66571334de2da23cee39fca
confett0 <118047961+confett0@users.noreply.github.com>
2025-12-10 17:43:30 +0100

    Fix settings modal styling

--- src/components/ColorPicker.jsx
+++ src/components/ColorPicker.jsx
@@ -1,3 +1,12 @@
 export default function ColorPicker() {
-    
+    return (
+        <div className="color-picker">
+            <fieldset>
+                <input type="radio" name="color" id="coral" />
+                <input type="radio" name="color" id="turquoise" />
+                <input type="radio" name="color" id="lilac" />
+            </fieldset>
+        </div>
+    )
+
 }
