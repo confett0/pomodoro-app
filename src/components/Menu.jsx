565171915bd4c2c037c0fdea670888497c787dec
confett0 <118047961+confett0@users.noreply.github.com>
2026-02-22 17:40:36 +0100

    Add keyboard navigation to menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,48 @@
+import { useRef } from "react";
+
 export default function Menu({ tabs, activeTab, handleTabChange }) {
+  const buttonRefs = useRef({});
+  const handleKeyDown = (e, currentIndex) => {
+    let nextIndex;
+
+    switch (e.key) {
+      case "ArrowRight":
+        e.preventDefault();
+        nextIndex = (currentIndex + 1) % tabs.length;
+        break;
+      case "ArrowLeft":
+        e.preventDefault();
+        nextIndex = (currentIndex - 1 + tabs.length) % tabs.length;
+        break;
+      case "Home":
+        e.preventDefault();
+        nextIndex = 0;
+        break;
+      case "End":
+        e.preventDefault();
+        nextIndex = tabs.length - 1;
+        break;
+      default:
+        return;
+    }
+
+    handleTabChange(tabs[nextIndex]);
+    buttonRefs.current[tabs[nextIndex]]?.focus(); // move focus
+  };
+
   return (
-    <nav>
-      <ul>
-        {tabs.map((label) => (
+    <nav aria-label="Session type selection">
+      <ul role="tablist">
+        {tabs.map((label, index) => (
           <li role="presentation" key={label}>
             <button
               role="tab"
+              ref={(el) => (buttonRefs.current[label] = el)}
               aria-selected={activeTab === label}
-              //tabIndex={activeTab === label ? 0 : -1}
               className={activeTab === label ? "menu-tab active" : "menu-tab"}
+              tabIndex={activeTab === label ? 0 : -1}
               onClick={() => handleTabChange(label)}
+              onKeyDown={(e) => handleKeyDown(e, index)}
             >
               {label}
             </button>
